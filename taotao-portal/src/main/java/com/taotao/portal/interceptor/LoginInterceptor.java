8a779258e5d7582ffa7d68e73b17e9da5142c039
Andy <Andy@DESKTOP-NQ38TIN.mshome.net>
2020-04-17 14:21:42 +0800

    购物车完成，购物车商品修改数量功能没有做

---
+++ taotao-portal/src/main/java/com/taotao/portal/interceptor/LoginInterceptor.java
@@ -0,0 +1,50 @@
+package com.taotao.portal.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import com.taotao.common.uitl.CookieUtils;
+import com.taotao.pojo.TbUser;
+import com.taotao.portal.service.impl.UserServiceImpl;
+
+public class LoginInterceptor implements HandlerInterceptor {
+	@Autowired
+	private UserServiceImpl userService;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		//在Handler执行之前处理，返回值决定Handler是否执行，true执行，false不执行；
+		//判断用户是否登入，1从cookie中取token；
+		String token = CookieUtils.getCookieValue(request, "TT_TOKEN");
+		//2根据token换取用户信息，需要调用sso系统接口；
+		TbUser user = userService.getUserByToken(token);
+		//如果取不到用户信息
+		if (null == user) {
+			//需要跳转到登陆页面，把用户请求url作为参数传递给登入页面;
+			response.sendRedirect(userService.SSO_BASE_URL + userService.SSO_PAGE_LOGIN 
+					+ "?redirect=" + request.getRequestURL());
+			//返回false
+			return false;
+		}
+		//取到用户信息，放行；
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		//在Handler执行之后，返回ModelAndView之前
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		//返回ModelAndView之后执行
+
+	}
+
+}
